5e2faae437696e9a1ee06cff7b1787f6334f8f84
Simon Eddy <simon@simoneddy.com.au>
2021-04-10 17:41:17 +1000

    more robodennis commentary

--- src/Plugins/RoboDennis/data/playerComments.php
+++ src/Plugins/RoboDennis/data/playerComments.php
@@ -3,21 +3,39 @@ return [
     'kick' => [
         '%s kicked that like my wife, and she has no legs.',
         'a punt supreme from %s',
-        '%s with a wobbly kick'
+        '%s with a wobbly kick',
+        '%s gives it a mignty roost',
+        '%s kicks it up to Voyager II',
+        '%s spears it down the line like my wife\'s credit card',
+        '%s kicked that clockwise when he wanted to go straight ahead.',
+        'by foot from %s',
+        '%s kicks the teeth out of it',
+        'kicked by %s',
     ],
     'handball' => [
         '%s\'s quick handball',
         'a zinger of a handball from %s',
         'handpass from %s',
-        '%s gives the trouble to someone else.'
+        '%s gives the trouble to someone else.',
+        '%s gets rid of it like it\'s my wife\'s credit card.',
+        'quick handpass from %s',
+        'looping pass from %s',
+        '%s goes by hand',
+        '%s just gets the pass away before he\'s consumed by the forewarned-coming storm',
+        'flicked ahead by %s'
     ],
     'hitout' => [
         '%s with some tapwork that would make Mario jealous.',
         '%s tapped that like my wife\'s credit card',
+        'tapped by %s',
+        '%s wins the ruck battle',
+        '%s smashes the ball into orbit.'
     ],
     'clearance' => [
         '%s extracts it from the slime.',
-        'extracted by %s'
+        'extracted by %s',
+        '%s gets the clearance',
+        '%s gets more clearances than my wife\'s credit card.'
     ],
     'mark' => [
         'a good grab from big %s',
@@ -25,14 +43,15 @@ return [
         '%s with the grab',
         '%s grabbed that like my wife\'s credit card.',
         'marked by %s',
-        ''
+        '%s with the safest pair of hands this side of old gaffa tape.'
     ],
     'goal' => [
         '%s kicks the major',
         '%s threads the needle like my wife\'s credit card.',
         'what a goal from %s',
         'straight over the umpire\'s hat, and it\'s a big hat.',
-        'just like the NBN %s is never off line'
+        'just like the NBN, %s is never off line',
+        'I don\'t know how %s put that through, so it\'s just as well I\'m not in charge of all reality.'
     ],
     'behind' => [
         '%s misses the shot',
@@ -42,4 +61,13 @@ return [
         '%s just misses to the side',
         'the umpire\'s hat is big, but %s can\' find the runway',
     ],
+    'spoil' => [
+        '%s fists that like Ace Ventura in a pygmy village.',
+        'spoiled by %s',
+        'fisted away by %s',
+        '%s with the spoil',
+        'thumped away by %s',
+        'punched away by %s',
+        '%s gets a fist to it in the nick of time',
+    ]
 ];
