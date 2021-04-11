70d70b730630134c43b344b30e8864dff229356b
Simon Eddy <simon@simoneddy.com.au>
2021-04-11 19:12:46 +1000

    scaffolding playerratings

--- src/Entities/Attributes/PlayerRatings.php
+++ src/Entities/Attributes/PlayerRatings.php
@@ -1,6 +1,35 @@
 <?php
 namespace Phooty\Entities\Attributes;
 
-class PlayerRatings
+class PlayerRatings implements \ArrayAccess
 {
+    public function __construct(private array $ratings)
+    {
+        
+    }
+
+    public function ratingNames()
+    {
+        return array_keys($this->ratings);
+    }
+
+    public function offsetExists($offset)
+    {
+        
+    }
+
+    public function offsetGet($offset)
+    {
+        return $this->ratings[$offset] ?? null;
+    }
+
+    public function offsetSet($offset, $value)
+    {
+        
+    }
+
+    public function offsetUnset($offset)
+    {
+        
+    }
 }
--- tests/spec/Entities/Attributes/PlayerRatingsSpec.php
+++ tests/spec/Entities/Attributes/PlayerRatingsSpec.php
@@ -7,8 +7,21 @@ use PhpSpec\ObjectBehavior;
 
 class PlayerRatingsSpec extends ObjectBehavior
 {
+    function let()
+    {
+        $this->beConstructedWith([
+            'speed' => 9.1
+        ]);
+    }
+    
+
     function it_is_initializable()
     {
         $this->shouldHaveType(PlayerRatings::class);
+    }
+
+    function it_contains_ratings_that_can_influence_rng()
+    {
+        $this->offsetGet('speed')->shouldBeFloat();
     }
 }
